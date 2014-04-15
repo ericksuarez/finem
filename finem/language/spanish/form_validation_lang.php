6068b16b098e3e28da11d253e2f5c079266f7b79
Julio Márquez <jmarquez@estrategiasdigitales.com.mx>
2014-04-15 12:39:22 -0500

    lenguaje

---
+++ finem/language/spanish/form_validation_lang.php
@@ -0,0 +1,33 @@
+<?php
+
+$lang['required'] 			= "El campo %s es obligatorio.";
+$lang['isset']				= "El campo %s debe contener un valor.";
+$lang['valid_email']		= "El campo %s debe contener una dirección de email válida.";
+$lang['valid_emails'] 		= "El campo %s debe contener todas las direcciones de email válidas.";
+$lang['valid_url'] 			= "El campo %s debe contener una URL válida.";
+$lang['valid_ip'] 			= "El campo %s debe contener una IP válida.";
+$lang['min_length']			= "El campo %s debe tener al menos %s caracteres de longitud.";
+$lang['max_length']			= "El campo %s no puede exceder los %s caracteres de longitud.";
+$lang['exact_length']		= "El campo %s debe tener exactamente %s caracteres de longitud.";
+$lang['alpha']				= "El campo %s debe contener sólo caracteres alfabéticos.";
+$lang['alpha_numeric']		= "El campo %s debe contener sólo caracteres alfa-numéricos.";
+$lang['alpha_dash']			= "El campo %s debe contener sólo caracteres alfa-numéricos, guiones bajos y guiones.";
+$lang['numeric']			= "El campo %s debe contener sólo números.";
+$lang['is_numeric']			= "El campo %s debe contener sólo caracteres numéricos.";
+$lang['integer']			= "El campo %s debe contener un número entero.";
+$lang['regex_match']		= "El campo %s no esta en formato correcto.";
+$lang['matches']			= "El campo %s no coincide con el campo %s.";
+$lang['is_unique'] 			= "Por favor elija otro valor para el campo %s ya que debe ser único en la base de datos.";
+$lang['is_natural']			= "El campo %s debe contener sólo números positivos.";
+$lang['is_natural_no_zero']	= "El campo %s debe contener un número mayor que cero.";
+$lang['decimal']			= "El campo %s debe contener un número decimal.";
+$lang['less_than']			= "El campo %s debe contener un número menor a %s.";
+$lang['greater_than']		= "El campo %s debe contener un número mayor a %s.";
+
+// FUNCIONES EXTENDIDAS.
+$lang['valida_fechas_unix']		= "El campo %s no contiene un formato válido de fecha.(aaaa-mm-dd)";
+$lang['valida_fechas_esp']		= "El campo %s no contiene el formato válido de fecha(DD-MM-AAAA) separado por guiones medios.";
+$lang['valida_moneda']		= "El campo %s no contiene un formato válido de moneda.";
+
+/* End of file form_validation_lang.php */
+/* Location: ./system/language/spanish/form_validation_lang.php */
